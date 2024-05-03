b80429d31a99e6cb65d9c8cf16c736d158d4be9a
UfoMc <99866680+UfoMc@users.noreply.github.com>
2024-05-03 21:39:48 +0200

    Update Main.java

--- matga/Main.java
+++ matga/Main.java
@@ -12,37 +12,10 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-
         final Boot boot = new Boot().enable();
 
-        Runtime.getRuntime().addShutdownHook(new Thread(boot::disable));
-
-        LoadingCache<Integer> cache = new CacheBuilder<Integer>()
-                .setExpireAfter(4)
-                .setTimeStamp(TimeUnit.SECONDS)
-                .setUpdateDelay(5)
-                .setMaxSize(2)
-                .build(new CacheImpMethods<>() {
-            @Override
-            public Integer fetch(String key) {
-                return null;
-            }
-
-            @Override
-            public void write(String key, Integer value) {
-
-            }
-        });
-
-
-
-        while (true) {
-            new Scanner(System.in).nextLine();
-        }
-
         //disable
-
-
+        Runtime.getRuntime().addShutdownHook(new Thread(boot::disable));
     }
 
 }
